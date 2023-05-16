647405a439e592ec3805b78e3eefea90c2fa1487
Niranjan Rath <niranjan.rath@gmail.com>
2023-05-17 00:49:40 +0200

    updated some with some implementations

--- twiddle-api/src/main/java/com/github/innovationforge/web/UserController.java
+++ twiddle-api/src/main/java/com/github/innovationforge/web/UserController.java
@@ -1,12 +1,9 @@
 package com.github.innovationforge.web;
 
-import com.github.innovationforge.model.User;
 import com.github.innovationforge.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
